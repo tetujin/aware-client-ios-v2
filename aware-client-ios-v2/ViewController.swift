4986b766e9167b4f3d44c08df0becd8a5a3ad07c
Yuuki Nishiyama <tetujin@ht.sfc.keio.ac.jp>
2019-02-27 12:42:23 +0200

    Initial Commit

---
+++ aware-client-ios-v2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  aware-client-ios-v2
+//
+//  Created by Yuuki Nishiyama on 2019/02/27.
+//  Copyright © 2019 Yuuki Nishiyama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ aware-client-ios-v2Tests/aware_client_ios_v2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  aware_client_ios_v2Tests.swift
+//  aware-client-ios-v2Tests
+//
+//  Created by Yuuki Nishiyama on 2019/02/27.
+//  Copyright © 2019 Yuuki Nishiyama. All rights reserved.
+//
+
+import XCTest
+@testable import aware_client_ios_v2
+
+class aware_client_ios_v2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
